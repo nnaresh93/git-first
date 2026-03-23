124efa6bcaa084d92e17123297060eabe3e3d103
root <root@ip-172-31-32-63.ap-south-1.compute.internal>
2026-03-23 11:47:38 +0000

    new-update

--- msg.java
+++ msg.java
@@ -6,7 +6,6 @@ public class msg {
 
     public static void greetUser(String name) {
         System.out.println("Hello, " + name + "! Welcome to Naresh.");
-        System.out.println("Hello, " + name + "! Welcome to kalyan.");
-        System.out.println("Hello, " + name + "! Welcome to charan.");
-    }
+	System.out.println("Hello, " + name + "! Welcome to kalyan.");
+	System.out.println("Hello, " + name + "! Welcome to charan.");    }
 }
