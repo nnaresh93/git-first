d3c6afd2cba25049a9eb2ff5c998fdfe5563d590
nnaresh93 <nnaresh1994@gmail.com>
2026-03-23 15:41:51 +0530

    Update msg.java

--- msg.java
+++ msg.java
@@ -4,6 +4,7 @@ let name = "Naresh";
 
 function greetUser() {
     console.log("Hello, " + name + "! Welcome to JavaScript.");
+    console.log("Hello, " + name + "! Welcome to Naresh.");
 }
 
 greetUser();
